c66dd44306a8721ad0d3f44d576528e6f4dedae9
yohei takara <b195.clo5566@gmail.com>
2017-01-03 15:41:57 +0900

    test

--- spec/demoSpec.js
+++ spec/demoSpec.js
@@ -1,5 +1,9 @@
 describe("demoTest", function() {
     it("demo getName", function() {
         expect(testTarget.name).toEqual("jasmine-test");
-    })
+    });
+    
+    it("demo テスト失敗", function() {
+        expect(testTarget.name).toEqual("");
+    });
 });
